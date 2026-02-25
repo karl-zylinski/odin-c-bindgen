e316f3db78789b8344020deaf277978432c20f32
Alex Davis <xandaron@gmail.com>
2026-02-25 15:29:36 +0000

    Fix for issue #116

--- test/src/test.h
+++ test/src/test.h
@@ -1,36 +1,6 @@
-#include <stdarg.h>
-
-#define TEST unsigned char
-
-typedef signed long   Int64;
-typedef unsigned long UInt64;
-
-typedef char testType[2];
-
-typedef void (*myLogImpl)(const char* fmt, ...);
-
-typedef void (myLogImpl2)(const char* fmt, ...);
-
-struct MyVtable {
-  myLogImpl    logger;
-  myLogImpl2*  logger2;
-  myLogImpl*   logger3;
-  myLogImpl2** logger4;
+enum Flags {
+	Flags_None = 0,
+	Flags_Flag1 = 1,
+	Flags_Flag2 = 2,
+	Flags_SpecialValue = 0xFFFF,
 };
-
-void test1(myLogImpl log);
-
-void test2(myLogImpl2* log);
-
-void test3(myLogImpl* log);
-
-void test4(myLogImpl2** log);
-
-char constArray(const char arr[2]);
-
-char typedef_test(testType arr);
-
-void functionNoProto();
-
-void functionProto(void);
-
